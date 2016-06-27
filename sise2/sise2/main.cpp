28f6ddf4ad7f0e3f1b16271e1b82c2e3ff0cc9b6
ringg <ringlan@gmail.com>
2016-06-27 22:04:19 +0200

    cross evaluation works now

--- sise2/sise2/main.cpp
+++ sise2/sise2/main.cpp
@@ -8,16 +8,11 @@
 #define QUE_COUNT 5
 #define NORMALIZE	// uncommenting this will normalise all element attributes
 #define STDDEV		// uncommenting this will divite all distances by their standard deviations
-#define DIST_FUNC 1	// 1 = euclidean, 2 = czebyszew, 3 = taxi 
+#define DIST_FUNC 1	// 1 = euclidean, 2 = czebyszew, 3 = taxi
 
 using namespace std;
 
-
-struct TestData
-{
-	vector<double> data;
-};
-
+typedef vector<double> TestData;
 
 struct DistType
 {
@@ -34,11 +29,11 @@ double DistanceSqEuc(TestData &l, TestData &r, vector<double> &stdDev)
 {
 	static double result;
 	result = 0;
-	for (int i = 0; i < l.data.size(); ++i)
+	for (int i = 0; i < l.size(); ++i)
 #ifdef STDDEV
-		result += (l.data[i] - r.data[i]) * (l.data[i] - r.data[i]) * stdDev[i];
+		result += (l[i] - r[i]) * (l[i] - r[i]) * stdDev[i];
 #else
-		result += (l.data[i] - r.data[i]) * (l.data[i] - r.data[i]);
+		result += (l[i] - r[i]) * (l[i] - r[i]);
 #endif // STDDEV
 
 	return result;
@@ -49,11 +44,11 @@ double DistanceCze(TestData &l, TestData &r, vector<double> &stdDev)
 {
 	static double result;
 	result = 0;
-	for (int i = 0; i < l.data.size(); ++i)
+	for (int i = 0; i < l.size(); ++i)
 #ifdef STDDEV
-		result = abs(l.data[i] - r.data[i]) * stdDev[i] > result ? abs(l.data[i] - r.data[i]) * stdDev[i] : result;
+		result = abs(l[i] - r[i]) * stdDev[i] > result ? abs(l[i] - r[i]) * stdDev[i] : result;
 #else
-		result = abs(l.data[i] - r.data[i]) > result ? abs(l.data[i] - r.data[i]) : result;
+		result = abs(l[i] - r[i]) > result ? abs(l[i] - r[i]) : result;
 #endif // STDDEV
 
 	return result;
@@ -64,11 +59,11 @@ double DistanceTax(TestData &l, TestData &r, vector<double> &stdDev)
 {
 	static double result;
 	result = 0;
-	for (int i = 0; i < l.data.size(); ++i)
+	for (int i = 0; i < l.size(); ++i)
 #ifdef STDDEV
-		result += abs(l.data[i] - r.data[i]) * stdDev[i];
+		result += abs(l[i] - r[i]) * stdDev[i];
 #else
-		result += abs(l.data[i] - r.data[i]);
+		result += abs(l[i] - r[i]);
 #endif // STDDEV
 
 	return result;
@@ -119,9 +114,8 @@ string GetMostCommonElement(priority_queue<DistType> &que)
 }
 
 
-bool TestNext(TestData &tested, double(*Distance)(TestData&, TestData&, vector<double>&), map<string, vector<TestData>> &info, map<string, vector<double>> &stdDev, vector<string> &keys, int maxCount)
+bool TestNext(TestData &tested, double(*Distance)(TestData&, TestData&, vector<double>&), map<string, vector<TestData>> &info, map<string, vector<double>> &stdDev, vector<string> &keys, int maxCount, string correct)
 {
-	static string correct;
 	static priority_queue<DistType> que;
 	que = priority_queue<DistType>();
 
@@ -137,117 +131,149 @@ bool TestNext(TestData &tested, double(*Distance)(TestData&, TestData&, vector<d
 	return correct == GetMostCommonElement(que);
 }
 
+static int correctGuesses = 0, guesses = 0;
+void TestSet(map<string, vector<TestData>>& learnData, map<string, vector<TestData>>& testData, vector<string>& keys)
+{
+	map<string, vector<double>> avgs;		// this contains the average values of elements by category
+	map<string, vector<double>> stdDev;		// this contains all standard deviations of values of elements by category
+	double(*Distance)(TestData&, TestData&, vector<double>&);  // function pointer: points at one of the distance functions
+	int k;
+
+	#if DIST_FUNC == 1
+	Distance = &DistanceSqEuc;
+	#elif DIST_FUNC == 2
+	Distance = &DistanceCze;
+	#elif DIST_FUNC == 3
+	Distance = &DistanceTax;
+	#endif
+
+	k = learnData[keys[0]][0].size();
+
+	// averages
+	for each(string key in keys)
+	{
+		if (avgs.find(key) == avgs.end())
+			avgs[key] = vector<double>(k, 0);
+		for each (TestData td in learnData[key])
+			for (int i = 0; i < td.size(); ++i)
+				avgs[key][i] += td[i] / learnData[key].size();
+	}
+
+	#ifdef NORMALIZE
+	for each(string key in keys)
+		for each (TestData td in learnData[key])
+			for (int i = 0; i < td.size(); ++i)
+				td[i] -= avgs[key][i];
+	#endif // NORMALIZE
+
+	#ifdef STDDEV
+	for each (string key in keys)
+	{
+		stdDev[key] = vector<double>(k, 0);
+
+		for each(TestData td in learnData[key])
+			for (int i = 0; i < k; ++i)
+				stdDev[key][i] += (avgs[key][i] - td[i]) * (avgs[key][i] - td[i]);
+
+		for (int i = 0; i < k; ++i)
+		{
+			stdDev[key][i] /= learnData[key].size();
+			stdDev[key][i] = 1 / stdDev[key][i]; // calculating inverse of std to avoid the division operation in the distance calculation
+		}
+	}
+	#endif // STDDEV
+
+	// guessing
+	for each (string key in keys)
+	{
+		for each (TestData td in testData[key])
+		{
+			correctGuesses += TestNext(td, Distance, learnData, stdDev, keys, QUE_COUNT, key);
+			guesses++;
+		}
+	}
+
+	//cout << "\nlearnData:";
+	//for each (string key in keys)
+	//{
+	//	cout << "\n" << key << ": " << learnData[key].size();
+	//}
+
+	//cout << "\ntestData:";
+	//for each (string key in keys)
+	//{
+	//	cout << "\n" << key << ": " << testData[key].size();
+	//}
+
+}
+
 
 int main()
 {
 	int k; // this is the ammount of attributes that describe each element
 	int n; // this is the ammount of elements for learning
-	int m; // this is the ammount of elements to recognise
+	int m;
 
-	map<string, vector<TestData>> info;		// this contains all elements used for learning
-	map<string, vector<double>> avgs;		// this contains the average values of elements by category
-	map<string, vector<double>> stdDev;		// this contains all standard deviations of values of elements by category
+	map<string, vector<TestData>> allData;
+	map<string, vector<TestData>> learnData;// this contains all elements used for learning
+	map<string, vector<TestData>> testData; // this contains all elements for guessing
 	vector<string> keys;					// this contains all names of categories
-	double(*Distance)(TestData&, TestData&, vector<double>&);  // function pointer: points at one of the distance functions
 
-															   // temp variables
+	// temp variables
 	TestData td;
 	string key;
 	double val;
-	int correctGuesses = 0;
-
-#if DIST_FUNC == 1
-	Distance = &DistanceSqEuc;
-#elif DIST_FUNC == 2
-	Distance = &DistanceCze;
-#elif DIST_FUNC == 3
-	Distance = &DistanceTAX;
-#endif
+	int firstPart, secondPart, thirdPart;
+	bool ok;
 
 
-	cin >> k >> n >> m;
+	cin >> k >> n;
 
 	// reading data for learning
 	for (int i = 0; i < n; ++i)
 	{
-		td.data.clear();
+		td.clear();
 		for (int j = 0; j < k; ++j)
 		{
 			cin >> val;
-			td.data.push_back(val);
+			td.push_back(val);
 		}
 
 		cin >> key;
-		info[key].push_back(td);			// adding read data into the collection
+		allData[key].push_back(td);		// adding read data into the collection
 
-		if (avgs.find(key) == avgs.end())	// if a new type of element appeared, add new average category
-		{
-			avgs[key].resize(k);
+		// if a new type of key appeared, add new category
+		ok = false;
+		for each (string k in keys)
+			if (k == key) ok = true;
+		if (ok == false)
 			keys.push_back(key);
-		}
-
-		for (int j = 0; j < k; ++j)			// add element info to average values by category collection
-			avgs[key][j] += td.data[j];
 	}
 
-	// calculate averages
-	for each (string key in keys)
-		for each (double val in avgs[key])
-			val /= info[key].size();
-
-#ifdef NORMALIZE
-	for each(string key in keys)
-		for each (TestData td in info[key])
-			for (int i = 0; i < td.data.size(); ++i)
-				td.data[i] /= avgs[key][i];
-#endif // NORMALIZE
-
-#ifdef STDDEV
-	// Calculate standard deviations for each category
-	for each (string key in keys)
+	//Testing by cross-validation
+	for (int i = 0; i < 3; ++i)
 	{
-		stdDev[key] = vector<double>(k, 0);
+		learnData.clear();
+		testData.clear();
 
-		for each(TestData td in info[key])
-			for (int i = 0; i < k; ++i)
-				stdDev[key][i] += (avgs[key][i] - td.data[i]) * (avgs[key][i] - td.data[i]);
-
-		for (int i = 0; i < k; ++i)
+		for each (string key in keys)
 		{
-			stdDev[key][i] /= info[key].size();
-			stdDev[key][i] = 1 / stdDev[key][i]; // calculating inverse of std to avoid the division operation in the distance calculation
-		}
-	}
-#endif // STDDEV
+			m = allData[key].size();
+			firstPart = (m + 2) / 3;
+			secondPart = 2 * firstPart;
+			thirdPart = m;
 
-	// reading data for guessing
-	for (int i = 0; i < m; ++i)
-	{
-		td.data.clear();
-		for (int j = 0; j < k; ++j)
-		{
-			cin >> val;
-			td.data.push_back(val);
+			for (int j = 0; j < m; ++j)
+			{
+				if ((i == 0 && j < firstPart) || (i == 1 && j >= firstPart && j < secondPart) || (i == 2 && j >= secondPart))
+					testData[key].push_back(allData[key][j]);
+				else
+					learnData[key].push_back(allData[key][j]);
+			}
 		}
 
-		correctGuesses += TestNext(td, Distance, info, stdDev, keys, QUE_COUNT);
+		TestSet(learnData, testData, keys);
 	}
 
-	cout << "\n\nCorrect guesses: " << correctGuesses << " out of " << m << ".\n";
+	cout << "\nCorrect guesses: " << correctGuesses << " out of " << guesses << ".";
 }
-
-/*
-maszyna wektorow podpierajacych da sie zrobic ponoc w 5 linijek,
-korzystajac z np libSVN (wolno)
-https://www.csie.ntu.edu.tw/~cjlin/libsvm/
-*/
-
-/*
-Dla ka¿dej klasy nale¿y policzyæ b³êdy I i II rodzaju. <TODO>
-Zwizualizowaæ wybrane rzuty zbiorów danych oraz granic decyzyjnych wybranych modeli (przynajmniej dla najskuteczniejszych modeli opartych odpowiednio o k-NN i SVM). <TODO>
-
-SVM
-Mo¿na wykorzystaæ gotow¹ implementacjê samego klasyfikatora, na przyk³ad libSVM - podczas oddawania projektu trzeba jednak umieæ wyjaœniæ jego zasadê dzia³ania.
-Przetestowaæ ró¿ne wartoœci parametrów oraz przynajmniej dwie ró¿ne metody przekszta³cenia j¹dra (kernel trick).
-Dla ka¿dej metody zbadaæ dzia³anie modelu dla ró¿nych wartoœci liczbowych parametrów i podj¹æ próbê wskazania wartoœci optymalnych.
-*/
